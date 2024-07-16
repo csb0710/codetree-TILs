20609a07c9027f989af3955bcb55afa2246ad83b
SeungBin Cha <56204009+csb0710@users.noreply.github.com>
2024-07-16 13:01:37 +0900

    Title: n x n 판 이동하기, Time: 188ms, Memory: 28MB, Status: Passed - CodeTree

--- 240716/n x n 판 이동하기/move-nxn-plates.py
+++ 240716/n x n 판 이동하기/move-nxn-plates.py
@@ -5,28 +5,24 @@ for i in range(n):
     arr[i] = list(map(int, input().split()))
 
 result = [[0]*n for _ in range(n)]
-q = []
-q.append([0, 0, 1])
 
-x = [1, 0]
-y = [0, 1]
+result[0][0] = 1
+arr[n-1][n-1] = 1
 
-while len(q) != 0:
-    temp = q.pop(0)
-    tempx = temp[0]
-    tempy = temp[1]
-    result[tempx][tempy] += temp[2]
-
-    if tempx == n-1 and tempy == n-1:
-        continue
-
-    for i in range(2):
-        tempxx = tempx + x[i] * arr[tempx][tempy]
-        tempyy = tempy + y[i] * arr[tempx][tempy]
-
-        if tempxx < 0 or tempxx >= n or tempyy < 0 or tempyy >= n:
-            continue
-
-        q.append([tempxx, tempyy, result[tempx][tempy]])
-
+for i in range(n):
+    for j in range(i, n):
+        tempx = i + arr[i][j]
+        tempy = j + arr[i][j]
+        if tempx < n:
+            result[tempx][j] += result[i][j]
+        if tempy < n:
+            result[i][tempy] += result[i][j]
+    for j in range(i+1, n):
+        tempx = j + arr[j][i]
+        tempy = i + arr[j][i]
+        if tempx < n:
+            result[tempx][i] += result[j][i]
+        if tempy < n:
+            result[j][tempy] += result[j][i]
+  
 print(result[n-1][n-1])

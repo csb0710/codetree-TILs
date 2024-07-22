eaeb1b11032a9843b42ed40a8efc0ada485fa36f
SeungBin Cha <56204009+csb0710@users.noreply.github.com>
2024-07-23 00:25:25 +0900

    Title: 계단 오르기 2, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240723/계단 오르기 2/climbing-stairs-2.py
@@ -0,0 +1,19 @@
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [[0] * n for _ in range(4)]
+
+dp[0][1] = arr[1]
+dp[1][0] = arr[0]
+dp[2][1] = arr[0] + arr[1]
+
+result = 0
+for i in range(2, n):
+    dp[0][i] = dp[0][i-2] + arr[i]
+    dp[1][i] = max(dp[0][i-1], dp[1][i-2]) + arr[i]
+    dp[2][i] = max(dp[1][i-1], dp[2][i-2]) + arr[i]
+    dp[3][i] = dp[3][i-1] + arr[i]
+
+for i in range(4):
+    result = max(result, dp[i][n-1])
+
+print(result)
